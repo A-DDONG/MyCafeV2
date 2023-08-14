65d4fa03b0038e56c54e5bcdf4488ee460add91c
A-DDONG <ssc5924@gmail.com>
2023-08-14 17:10:59 +0900

    mycafe

--- src/layout/MainRouter.js
+++ src/layout/MainRouter.js
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import CafeMain from "../comps/CafeMain";
 import CafeInput from "../comps/CafeInput";
 import CafeList from "../comps/CafeList";
@@ -7,24 +7,20 @@ import Home from "../comps/Home";
 import App from "../App";
 
 const MainRouter = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        { path: "", element: <Home /> },
-        {
-          path: "/recipe",
-          element: <CafeMain />,
-          children: [
-            { path: "insert", element: <CafeInput /> },
-            { path: "list", element: <CafeList /> },
-            { path: "update/:id", element: <CafeUpdate /> },
-          ],
-        },
-      ],
-    },
-  ]);
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="recipe" element={<CafeMain />}>
+            <Route path="insert" element={<CafeInput />} />
+            <Route path="list" element={<CafeList />} />
+            <Route path="update/:id" element={<CafeUpdate />} />
+          </Route>
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 };
+
 export default MainRouter;
